47555aad13938dba31ab7a5d8be9c29c83b88530
Andres Bernal <publiminal@gmail.com>
2022-03-18 04:59:55 -0400

    added some event listeners

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function noHighlight(el) {
     el.classList.remove('over')
     el.classList.add('no-over')
     const overInfo = el.querySelector('.overInfo')
-   if(overInfo) {overInfo.remove() }
+    if (overInfo) { overInfo.remove() }
     // console.log(overInfo)
 }
 
@@ -26,14 +26,15 @@ function doContentEditable(ev) {
     if (isP(ev)) {
         ev.srcElement.contentEditable = true
         console.log('entro a crear', ev.srcElement.dataset.info)
-        
+
         if (ev.srcElement.dataset.info) {
             ev.srcElement.setAttribute('data-info', true)
 
             const overInfo = document.createElement('div')
             overInfo.classList.add('overInfo')
             overInfo.textContent = 'double click to edit text'
-            ev.srcElement.parentNode.insertBefore(overInfo, ev.srcElement )
+            ev.srcElement.parentNode.insertBefore(overInfo, ev.srcElement)
+            // overInfo.removeEventListener('mouseover' ,onMouseOver )
             // 
             // console.log(ev.srcElement.parentNode)
         }
@@ -43,11 +44,18 @@ function doContentEditable(ev) {
 
 function onMouseOver(ev) {
     const isBody = ev.srcElement.localName === 'body'
-    const isOverInfo = document.querySelector('.overInfo')
-    if (!isBody && !isOverInfo ) { setHighlight(ev.target) }
+    const isOverInfo = document.activeElement.querySelector('.overInfo')
     ev.srcElement.setAttribute('data-info', false)
-        // const infoSpan = ev.target
-    doContentEditable(ev)
+    if (!isBody || !isOverInfo) {
+        setHighlight(ev.target)
+        // doContentEditable(ev)
+    }
+    if (!isOverInfo ) {
+        // setHighlight(ev.target)
+        doContentEditable(ev)
+    }
+
+    console.log(isOverInfo)
 
 }
 
@@ -63,7 +71,6 @@ function onkeyDown(ev) {
 function onDblclick(ev) {
     console.log(ev.target.textContent)
 }
-
 
 document.addEventListener('mouseover', onMouseOver)
 document.addEventListener('mouseout', onMouseOut)
